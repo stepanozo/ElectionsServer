dca4d685f3d8aca289846358ba3e42c7177fe688
stepa <stepa@ACER-STEPAN>
2024-06-22 18:54:00 +0300

    Тестовый коммит 2

--- src/test/java/stepanozo/stepanozo/StepanozoApplicationTests.java
+++ src/test/java/stepanozo/stepanozo/StepanozoApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//Тестовый комментарий
 //Тестовый класс
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
